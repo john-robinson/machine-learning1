2a23cc4085be3577a945f3363e67c3dc103640f8
Alrahel Badei <badei.alrahel@student.uliege.be>
2022-10-13 14:59:11 +0200

    qda.py end
    Co-authored-by: louiscolsonUlg <louiscolsonUlg@users.noreply.github.com>
    Co-authored-by: John Robinson <john-robinson@users.noreply.github.com>

--- code/qda.py
+++ code/qda.py
@@ -6,12 +6,14 @@ Project 1 - Classification algorithms
 #! /usr/bin/env python
 # -*- coding: utf-8 -*-
 
+from pickle import TRUE
+from tkinter import FALSE
 import numpy as np
 
 from sklearn.base import BaseEstimator, ClassifierMixin
 
 from sklearn.model_selection import train_test_split
-
+from scipy.stats import multivariate_normal
 
 
 class QuadraticDiscriminantAnalysis(BaseEstimator, ClassifierMixin):
@@ -44,6 +46,7 @@ class QuadraticDiscriminantAnalysis(BaseEstimator, ClassifierMixin):
             raise ValueError("The number of samples differs between X and y")
 
         self.lda = lda
+        self.classes = np.unique(y)
 
         
         # ====================
@@ -52,20 +55,11 @@ class QuadraticDiscriminantAnalysis(BaseEstimator, ClassifierMixin):
         
         # covariance matrix of X for each class
         if self.lda:
-            self.covs = np.array([np.cov(X[y == b], rowvar=False) for b in np.unique(y)])
+            self.covs = np.array([np.cov(X, rowvar=False) for _ in np.unique(y)])
         else: # QDA
             self.covs = np.array([np.cov(X[y == b], rowvar=False) for b in np.unique(y)])
 
-        print("Covariance matrices")
-        for mat in self.covs:
-            print(mat)
-        
-        print("Mean vectors")
-        i = 0
-        for mean in self.means:
-            print(i, " ", mean)
-            i+=1
-            
+
         return self
 
     def predict(self, X):
@@ -81,14 +75,8 @@ class QuadraticDiscriminantAnalysis(BaseEstimator, ClassifierMixin):
         y : array of shape = [n_samples]
             The predicted classes, or the predict values. 
         """
-        # Vecteur de classe
-        # ====================
-        # TODO your code here.
-        # ====================
-
         # argmax(k, self.predict_proba(X))
-
-        pass
+        return np.argmax(self.predict_proba(X), axis=1)
 
     def predict_proba(self, X):
         """Return probability estimates for the test data X.
@@ -105,24 +93,36 @@ class QuadraticDiscriminantAnalysis(BaseEstimator, ClassifierMixin):
             by lexicographic order.
         """
 
-        # ====================
-        # TODO your code here.
-        # ====================
+        # fk(x)
+        var = np.array([multivariate_normal(mean=self.means[k], cov=self.covs[k]) for k in range(len(self.classes))])
+            
+            
+        # initial prior is uniform
+        prior = np.full(shape=len(self.classes), fill_value=1/len(self.classes))
+        
+        proba_per_class = np.empty((X.shape[0], len(self.classes)))
+        for x in range(len(X)):
+            for k in range(len(self.classes)):
+                # calculer P(y = k|x)
+                proba_per_class[x][k] = prior[k] * var[k].pdf(X[x]) / np.sum([prior[i] * var[i].pdf(X[x]) for i in range(len(self.classes))])
+                
+        return proba_per_class
 
-        pass
 
 if __name__ == "__main__":
     from data import make_dataset1
+    from data import make_dataset2
     from plot import plot_boundary
     TRAINING= 1200
     TESTING = 300
     n_points = 1500
     random_state = 5
+    LDA = False
     X, y = make_dataset1(n_points, random_state=random_state)
     
     # sample 1200 points to use as training set
     X_train, X_test, y_train, y_test = train_test_split(X, y, train_size=TRAINING, test_size=TESTING, random_state = random_state)
     
     ncl = QuadraticDiscriminantAnalysis()
-    ncl.fit(X_train, y_train)
-    # print(ncl.means[0])
+    ncl.fit(X_train, y_train, lda = LDA)
+    plot_boundary("boundary" + "LDA" if LDA else "boundary" + "QDA" , ncl, X_test, y_test)
